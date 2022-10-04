48cba4e878a513be396ded248160efa0b47da823
AlexOnex <a.schwartzmann@hotmail.com>
2022-10-04 08:16:24 -0300

    General update

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -74,11 +74,10 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
-
 (use-package! copilot
   :hook (prog-mode . copilot-mode)
-  :bind (("C-a" . 'copilot-accept-completion-by-word)
-         ("C-a" . 'copilot-accept-completion-by-word)
+  :bind (("C-TAB" . 'copilot-accept-completion-by-word)
+         ("C-<tab>" . 'copilot-accept-completion-by-word)
          :map copilot-completion-map
-         ("C-a" . 'copilot-accept-completion)
-         ("C-a" . 'copilot-accept-completion)))
+         ("<tab>" . 'copilot-accept-completion)
+         ("TAB" . 'copilot-accept-completion)))
