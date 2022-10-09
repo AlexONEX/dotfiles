654555f4fdbef48992551718a9fe5cd1ffd2b9d0
AlexOnex <a.schwartzmann@hotmail.com>
2022-10-09 19:07:44 -0300

    Updating Scripts

--- Scripts/executable_uDotFiles.sh
+++ Scripts/executable_uDotFiles.sh
@@ -1,2 +1,1 @@
-chezmoi add $HOME/.aliases $HOME/.zshrc $HOME/.config/alacritty $HOME/.config/lvim $HOME/.doom.d 
-chezmoi add $HOME/Scripts/ $HOME/.oh-my-zsh/
+chezmoi add $HOME/.aliases $HOME/.zshrc $HOME/.config/alacritty $HOME/.config/lvim $HOME/.doom.d $HOME/Scripts/ $HOME/.oh-my-zsh/
--- Scripts/executable_updatePipPackages.sh
+++ Scripts/executable_updatePipPackages.sh
@@ -1,3 +1,5 @@
 #YT-DLP
 python3 -m pip install --force-reinstall https://github.com/yt-dlp/yt-dlp/archive/master.tar.gz
 
+#lightnovel-crawler
+python3 -m pip install --force-reinstall lightnovel-crawler
