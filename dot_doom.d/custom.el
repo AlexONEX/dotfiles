68e28b7b03cc8683774589d6aac1a04aed64fdc1
AlexOnex <a.schwartzmann@hotmail.com>
2022-12-03 17:27:15 -0300

    Updating Dots. Change nvim to lvim. Lvim can't support LTex yet.

--- dot_doom.d/custom.el
+++ dot_doom.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" "ae426fc51c58ade49774264c17e666ea7f681d8cae62570630539be3d06fd964" "8b6506330d63e7bc5fb940e7c177a010842ecdda6e1d1941ac5a81b13191020e" "1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" default)))
+   '("7ea883b13485f175d3075c72fceab701b5bf76b2076f024da50dff4107d0db25" "7e068da4ba88162324d9773ec066d93c447c76e9f4ae711ddd0c5d3863489c52" "944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" "ae426fc51c58ade49774264c17e666ea7f681d8cae62570630539be3d06fd964" "8b6506330d63e7bc5fb940e7c177a010842ecdda6e1d1941ac5a81b13191020e" "1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00" "aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
