8f220793951fe10c4b93e0a0af1e8f0f527a6bd0
AlexONEX <a.schwartzmann@hotmail.com>
2023-05-04 15:10:17 -0300

    Redo nvim config

--- dot_config/nvim/after/plugin/treesitter.lua
+++ dot_config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = {"help", "cpp", "c", "lua", "vim", "help", "rust" },
+  ensure_installed = {"cpp", "c", "lua", "vim", "rust" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
