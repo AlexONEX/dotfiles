21824a40ba05a605757f9fe2a7d0fb9d8028482b
AlexONEX <a.schwartzmann@hotmail.com>
2023-07-17 02:46:55 -0300

    mend

--- dot_config/nvim/after/plugin/suda.lua
+++
@@ -1,7 +0,0 @@
--- :SudaRead Read file with sudo
--- :SudaWrite Write file with sudo
--- Define keybindings to use this plugin
--- <leader>rw :SudaWrite
--- <leader>rr :SudaRead
-vim.keymap.set("n", "<leader>rw", ":SudaWrite<CR>", {noremap = true})
-vim.keymap.set("n", "<leader>rr", ":SudaRead<CR>", {noremap = true})
