1bf9b1763a60fd12df26338ba8b111152d6a9206
AlexONEX <a.schwartzmann@hotmail.com>
2023-09-10 18:25:58 -0300

    Updated zsh, quote manager plugin

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,7 +1,3 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 
@@ -33,8 +29,8 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
-vim.opt.updatetime = 50
-vim.opt.colorcolumn = "80"
+vim.opt.updatetime = 75
+vim.opt.colorcolumn = "75"
 
 vim.g.python3_host_prog = "/usr/bin/python3"
 vim.opt.spelllang = "es"
