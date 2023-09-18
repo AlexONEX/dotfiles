90ea9c1bf40f4a925ba62c7f90827dc1e437e720
AlexONEX <a.schwartzmann@hotmail.com>
2023-09-17 23:20:56 -0300

    Added linter

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,6 +2,12 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
+      eslint = {
+        settings = {
+          -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+          workingDirectory = { mode = "auto" },
+        },
+      },
       -- Ensure mason installs the server
       clangd = {
         keys = {
@@ -44,6 +50,24 @@ return {
         local clangd_ext_opts = require("lazyvim.util").opts("clangd_extensions.nvim")
         require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts }))
         return false
+      end,
+      eslint = function()
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          callback = function(event)
+            if not require("lazyvim.plugins.lsp.format").enabled() then
+              -- exit early if autoformat is not enabled
+              return
+            end
+
+            local client = vim.lsp.get_active_clients({ bufnr = event.buf, name = "eslint" })[1]
+            if client then
+              local diag = vim.diagnostic.get(event.buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id) })
+              if #diag > 0 then
+                vim.cmd("EslintFixAll")
+              end
+            end
+          end,
+        })
       end,
     },
   },
