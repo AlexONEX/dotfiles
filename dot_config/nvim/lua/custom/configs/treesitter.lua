4adcc6ab0f9bdd9b71a1dfb0ad91006c2f2a8408
AlexONEX <a.schwartzmann@hotmail.com>
2023-12-30 13:56:58 -0300

    custom config for treesitter

---
+++ dot_config/nvim/lua/custom/configs/treesitter.lua
@@ -0,0 +1,16 @@
+local options = {
+  ensure_installed = {
+    "lua",
+    "terraform",
+    "hcl",
+  },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options
