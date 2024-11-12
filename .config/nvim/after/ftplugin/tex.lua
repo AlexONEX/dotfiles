74626ca9fdd4f168c1ece20338e155cc1b4adf29
AlexONEX <a.schwartzmann@hotmail.com>
2024-11-12 20:54:06 -0300

    Added latex snippets, and corrected latex support in nvim

--- .config/nvim/after/ftplugin/tex.lua
+++ .config/nvim/after/ftplugin/tex.lua
@@ -1,23 +1,47 @@
--- Configuración general
-vim.opt_local.textwidth = 120
-vim.opt_local.wrap = true
-vim.opt_local.formatoptions:remove({ "o", "r" })
+-- Load VimTeX configuration if available
+if vim.fn.filereadable(vim.fn.stdpath("config") .. "/lua/config/vimtex.lua") then
+  local vimtex_config = require("config.vimtex")
+  vimtex_config.setup()
+end
 
--- Función para formatear y guardar
+-- buffer
+local function setup_buffer()
+  -- Configuración de texto y formato
+  vim.opt_local.textwidth = 120
+  vim.opt_local.wrap = true
+  vim.opt_local.linebreak = true
+  vim.opt_local.conceallevel = 2
+
+  vim.opt_local.formatoptions:remove { "o", "r" }
+
+  -- indent
+  vim.opt_local.expandtab = true
+  vim.opt_local.shiftwidth = 2
+  vim.opt_local.tabstop = 2
+  vim.opt_local.softtabstop = 2
+
+  -- Spell checking
+  vim.opt_local.spell = true
+  vim.opt_local.spelllang = "es,en"
+end
+
+-- Format and save
 local function format_and_save()
-	vim.lsp.buf.format()
-	vim.cmd("write")
+  vim.lsp.buf.format()
+  vim.cmd("write")
 end
 
--- Función para compilar LaTeX
-local function compile_latex()
-	vim.cmd("AsyncRun pdflatex -interaction=nonstopmode -synctex=1 %")
+-- Configurar keymaps genéricos para LaTeX
+local function setup_latex_keymaps()
+  -- Guardar y formatear
+  vim.keymap.set("n", "<C-s>", format_and_save, { buffer = true, silent = true })
+
+  -- Snippets y completion (si no usas VimTeX para esto)
+  vim.keymap.set("i", ";;", "\\", { buffer = true })
+  vim.keymap.set("i", "$$", "$$ $$<left><left><left>", { buffer = true })
 end
 
--- Crear comandos de usuario
+setup_buffer()
+setup_latex_keymaps()
+
 vim.api.nvim_create_user_command("FormatAndSaveLatex", format_and_save, {})
-vim.api.nvim_create_user_command("CompileLatex", compile_latex, {})
-
--- Mapeo de teclas
-vim.api.nvim_buf_set_keymap(0, "n", "<C-s>", ":FormatAndSaveLatex<CR>", { noremap = true, silent = true })
-vim.api.nvim_buf_set_keymap(0, "n", "<F9>", ":CompileLatex<CR>", { noremap = true, silent = true })
---
+++ .config/nvim/lua/config/vimtex.lua
@@ -0,0 +1,121 @@
+-- ~/.config/nvim/lua/config/vimtex.lua
+local M = {}
+
+-- Configuración de keymaps específicos de VimTeX
+local function setup_keymaps()
+	local keymaps = {
+		-- Compilación
+		{ "n", "<F9>", "<plug>(vimtex-compile)", { buffer = true } },
+		{ "n", "<leader>ll", "<cmd>VimtexCompile<CR>", { buffer = true } },
+		-- Visualización
+		{ "n", "<leader>lv", "<cmd>VimtexView<CR>", { buffer = true } },
+		{ "n", "<leader>le", "<cmd>VimtexErrors<CR>", { buffer = true } },
+		-- TOC
+		{ "n", "<leader>lt", "<cmd>VimtexTocToggle<CR>", { buffer = true } },
+		-- Limpieza
+		{ "n", "<leader>lc", "<cmd>VimtexClean<CR>", { buffer = true } },
+		-- Otros
+		{ "n", "<leader>li", "<cmd>VimtexInfo<CR>", { buffer = true } },
+		{ "n", "<leader>lk", "<cmd>VimtexStop<CR>", { buffer = true } },
+		{ "n", "<leader>lK", "<cmd>VimtexStopAll<CR>", { buffer = true } },
+		{ "n", "<leader>lg", "<cmd>VimtexLog<CR>", { buffer = true } },
+	}
+
+	for _, map in ipairs(keymaps) do
+		vim.keymap.set(unpack(map))
+	end
+end
+
+-- Configuración de autocommands específicos de VimTeX
+local function setup_autocommands()
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = "tex",
+		callback = function()
+			-- Configurar servidor para sync inverso
+			local nvim_server_file = "/tmp/vimtexserver.txt"
+			local file = io.open(nvim_server_file, "w")
+			if file then
+				file:write(vim.v.servername)
+				file:close()
+			end
+		end,
+	})
+end
+
+-- Configuración principal de VimTeX
+function M.setup()
+	-- Visor de PDF
+	vim.g.vimtex_view_method = "zathura"
+	vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
+
+	-- Compilador
+	vim.g.vimtex_compiler_method = "latexmk"
+	-- En tu vimtex.lua
+	vim.g.vimtex_compiler_latexmk = {
+		build_dir = "",
+		callback = 1,
+		continuous = 1,
+		executable = "latexmk",
+		hooks = {},
+		options = {
+			"-verbose",
+			"-file-line-error",
+			"-synctex=1",
+			"-interaction=nonstopmode",
+			"-pdf",
+		},
+	}
+
+	-- Agregar estas líneas para mejor diagnóstico
+	vim.g.vimtex_compiler_progname = "nvr"
+	vim.g.vimtex_compiler_enabled = 1
+	vim.g.vimtex_compiler_silent = 0
+	vim.g.vimtex_view_enabled = 1
+	vim.g.vimtex_log_verbose = 1
+
+	-- TOC
+	vim.g.vimtex_toc_config = {
+		name = "TOC",
+		layers = { "content", "todo", "include" },
+		resize = 1,
+		split_width = 30,
+		todo_sorted = 0,
+		show_help = 1,
+		show_numbers = 1,
+		mode = 2,
+	}
+
+	-- Detect main file
+	vim.g.vimtex_compiler_latexmk.root_patterns = { ".latexmkrc", "main.tex", ".git", "." }
+
+	-- QuickFix
+	vim.g.vimtex_quickfix_mode = 1
+	vim.g.vimtex_quickfix_enabled = 1
+	vim.g.vimtex_quickfix_ignore_filters = {
+		"Underfull",
+		"Overfull",
+		"specifier changed to",
+		"Package caption Warning",
+		"Package typearea Warning",
+	}
+
+	-- Sintaxis y características
+	vim.g.vimtex_syntax_enabled = 0 -- Usar TreeSitter
+	vim.g.vimtex_fold_enabled = 0
+	vim.g.vimtex_format_enabled = 1
+	vim.g.vimtex_complete_enabled = 1
+	vim.g.vimtex_mappings_disable = { ["n"] = { "K" } }
+
+	-- Configurar importer para incluir archivos
+	vim.g.vimtex_importer_enabled = 1
+
+	-- Configurar autocommands
+	setup_autocommands()
+	-- Configurar keymaps
+	setup_keymaps()
+end
+
+-- Exportar funciones útiles
+M.setup_keymaps = setup_keymaps
+
+return M
