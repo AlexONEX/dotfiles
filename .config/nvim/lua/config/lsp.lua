7868f50082736cbce60ae16460c7721ca5c0b844
AlexONEX <22077128+AlexONEX@users.noreply.github.com>
2025-04-06 22:40:13 -0300

    feat(nvim): use lsp attch for buffer behavior

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -6,93 +6,77 @@ local diagnostic = vim.diagnostic
 local lspconfig = require("lspconfig")
 local typescript_tools = require("typescript-tools")
 
-local utils = require("utils")
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("buf_behavior_conf", { clear = true }),
+  callback = function(event_context)
+    local client = vim.lsp.get_client_by_id(event_context.data.client_id)
+    -- vim.print(client.name, client.server_capabilities)
 
-local custom_attach = function(client, bufnr)
-  -- Mappings.
-  local map = function(mode, l, r, opts)
-    opts = opts or {}
-    opts.silent = true
-    opts.buffer = bufnr
-    keymap.set(mode, l, r, opts)
-  end
-
-  map("n", "gd", vim.lsp.buf.definition, { desc = "go to definition" })
-  map("n", "<C-]>", vim.lsp.buf.definition)
-  map("n", "K", function()
-    vim.lsp.buf.hover { border = "single", max_height = 25, max_width = 120 }
-  end)
-  map("n", "<C-k>", vim.lsp.buf.signature_help)
-  map("n", "<space>rn", vim.lsp.buf.rename, { desc = "varialbe rename" })
-  map("n", "[d", function()
-    diagnostic.jump { count = -1 }
-  end, { desc = "previous diagnostic" })
-  map("n", "]d", function()
-    diagnostic.jump { count = 1 }
-  end, { desc = "next diagnostic" })
-
-  map("n", "<space>ca", vim.lsp.buf.code_action, { desc = "LSP code action" })
-  map("n", "<space>wa", vim.lsp.buf.add_workspace_folder, { desc = "add workspace folder" })
-  map("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, { desc = "remove workspace folder" })
-  map("n", "<space>wl", function()
-    vim.print(vim.lsp.buf.list_workspace_folders())
-  end, { desc = "list workspace folder" })
-  map("n", "<leader>df", vim.diagnostic.open_float, { desc = "show diagnostics in float" })
-
-  -- Set some key bindings conditional on server capabilities
-  if client.server_capabilities.documentFormattingProvider then
-    map({ "n", "x" }, "<space>f", vim.lsp.buf.format, { desc = "format code" })
-  end
-
-  _G.copy_diagnostic_message = function()
-    local diagnostics = vim.diagnostic.get(0, { lnum = vim.fn.line(".") - 1 })
-    if #diagnostics > 0 then
-      local message = diagnostics[1].message
-      vim.fn.setreg("+", message) -- Copy to system clipboard
-      print("Diagnostic message copied to clipboard")
-    else
-      print("No diagnostic message at current line")
+    if not client then
+      return
     end
-  end
-  vim.api.nvim_buf_set_keymap(
-    0,
-    "n",
-    "<leader>dc",
-    ":lua _G.copy_diagnostic_message()<CR>",
-    { noremap = true, silent = true }
-  )
 
-  -- The below command will highlight the current variable and its usages in the buffer.
-  if client.server_capabilities.documentFormattingProvider and client.name ~= "lua_ls" then
-    vim.cmd([[
-      hi! link LspReferenceRead Visual
-      hi! link LspReferenceText Visual
-      hi! link LspReferenceWrite Visual
-    ]])
+    local bufnr = event_context.buf
 
-    local gid = api.nvim_create_augroup("lsp_document_highlight", { clear = true })
-    api.nvim_create_autocmd("CursorHold", {
-      group = gid,
-      buffer = bufnr,
-      callback = function()
-        lsp.buf.document_highlight()
-      end,
-    })
+    -- Mappings.
+    local map = function(mode, l, r, opts)
+      opts = opts or {}
+      opts.silent = true
+      opts.buffer = bufnr
+      keymap.set(mode, l, r, opts)
+    end
 
-    api.nvim_create_autocmd("CursorMoved", {
-      group = gid,
-      buffer = bufnr,
-      callback = function()
-        lsp.buf.clear_references()
-      end,
-    })
-  end
+    map("n", "gd", vim.lsp.buf.definition, { desc = "go to definition" })
+    map("n", "<C-]>", vim.lsp.buf.definition)
+    map("n", "K", function()
+      vim.lsp.buf.hover { border = "single", max_height = 25, max_width = 120 }
+    end)
+    map("n", "<C-k>", vim.lsp.buf.signature_help)
+    map("n", "<space>rn", vim.lsp.buf.rename, { desc = "varialbe rename" })
+    map("n", "<space>ca", vim.lsp.buf.code_action, { desc = "LSP code action" })
+    map("n", "<space>wa", vim.lsp.buf.add_workspace_folder, { desc = "add workspace folder" })
+    map("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, { desc = "remove workspace folder" })
+    map("n", "<space>wl", function()
+      vim.print(vim.lsp.buf.list_workspace_folders())
+    end, { desc = "list workspace folder" })
 
-  if vim.g.logging_level == "debug" then
-    local msg = string.format("Language server %s started!", client.name)
-    vim.notify(msg, vim.log.levels.DEBUG, { title = "Nvim-config" })
-  end
-end
+    -- Set some key bindings conditional on server capabilities
+    if client.server_capabilities.documentFormattingProvider and client.name ~= "lua_ls" then
+      map({ "n", "x" }, "<space>f", vim.lsp.buf.format, { desc = "format code" })
+    end
+
+    -- Disable ruff hover feature in favor of Pyright
+    if client.name == "ruff" then
+      client.server_capabilities.hoverProvider = false
+    end
+
+    -- Uncomment code below to enable inlay hint from language server, some LSP server supports inlay hint,
+    -- but disable this feature by default, so you may need to enable inlay hint in the LSP server config.
+    -- vim.lsp.inlay_hint.enable(true, {buffer=bufnr})
+
+    -- The blow command will highlight the current variable and its usages in the buffer.
+    if client.server_capabilities.documentHighlightProvider then
+      local gid = api.nvim_create_augroup("lsp_document_highlight", { clear = true })
+      api.nvim_create_autocmd("CursorHold", {
+        group = gid,
+        buffer = bufnr,
+        callback = function()
+          lsp.buf.document_highlight()
+        end,
+      })
+
+      api.nvim_create_autocmd("CursorMoved", {
+        group = gid,
+        buffer = bufnr,
+        callback = function()
+          lsp.buf.clear_references()
+        end,
+      })
+    end
+  end,
+  nested = true,
+  desc = "Configure buffer keymap and behavior based on LSP",
+})
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 
@@ -127,8 +111,6 @@ if utils.executable("pyright") then
 
   lspconfig.pyright.setup {
     --cmd = { "delance-langserver", "--stdio" },
-    on_attach = custom_attach,
-    -- capabilities = merged_capability,
     capabilities = merged_capability,
     pyright = {
       -- disable import sorting and use Ruff for this
@@ -161,7 +143,6 @@ end
 
 if utils.executable("ruff") then
   require("lspconfig").ruff.setup {
-    on_attach = custom_attach,
     capabilities = capabilities,
     init_options = {
       -- the settings can be found here: https://docs.astral.sh/ruff/editors/settings/
@@ -172,26 +153,8 @@ if utils.executable("ruff") then
   }
 end
 
--- Disable ruff hover feature in favor of Pyright
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
-  callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    -- vim.print(client.name, client.server_capabilities)
-
-    if client == nil then
-      return
-    end
-    if client.name == "ruff" then
-      client.server_capabilities.hoverProvider = false
-    end
-  end,
-  desc = "LSP: Disable hover capability from Ruff",
-})
-
 if utils.executable("ltex-ls") then
   lspconfig.ltex.setup {
-    on_attach = custom_attach,
     cmd = { "ltex-ls" },
     filetypes = { "text", "plaintex", "tex", "markdown" },
     settings = {
@@ -205,7 +168,6 @@ end
 
 if utils.executable("clangd") then
   lspconfig.clangd.setup {
-    on_attach = custom_attach,
     capabilities = capabilities,
     filetypes = { "c", "cpp", "cc" },
     flags = {
@@ -217,7 +179,6 @@ end
 -- set up bash-language-server
 if utils.executable("bash-language-server") then
   lspconfig.bashls.setup {
-    on_attach = custom_attach,
     capabilities = capabilities,
     filetypes = { "sh", "bash", "zsh" },
   }
@@ -227,7 +188,6 @@ end
 if utils.executable("lua-language-server") then
   -- settings for lua-language-server can be found on https://github.com/LuaLS/lua-language-server/wiki/Settings .
   lspconfig.lua_ls.setup {
-    on_attach = custom_attach,
     capabilities = capabilities,
     settings = {
       Lua = {
@@ -266,7 +226,6 @@ end
 
 if utils.executable("rust-analyzer") then
   lspconfig.rust_analyzer.setup {
-    on_attach = custom_attach,
     capabilities = capabilities,
     settings = {
       ["rust-analyzer"] = {
