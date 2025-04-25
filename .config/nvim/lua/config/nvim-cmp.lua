9dd0c68e9d45a2f340751e06999e96b7e4932039
AlexONEX <22077128+AlexONEX@users.noreply.github.com>
2025-04-24 23:51:56 -0300

    feat(nvim): migrate nvim-cmp to use mini.icons
    - Replace lspkind with mini.icons for completion icons
    - Update dependencies and lazy-loading configuration
    - Simplify completion menu formatting

--- .config/nvim/lua/config/nvim-cmp.lua
+++ .config/nvim/lua/config/nvim-cmp.lua
@@ -1,73 +1,70 @@
--- Setup nvim-cmp.
 local cmp = require("cmp")
-local lspkind = require("lspkind")
 
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      -- For `ultisnips` user.
-      vim.fn["UltiSnips#Anon"](args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert {
-    ["<Tab>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end,
-    ["<S-Tab>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end,
-    ["<CR>"] = cmp.mapping.confirm { select = true },
-    ["<C-e>"] = cmp.mapping.abort(),
-    ["<Esc>"] = cmp.mapping.close(),
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-  },
-  sources = {
-    { name = "nvim_lsp" }, -- For nvim-lsp
-    { name = "ultisnips" }, -- For ultisnips user.
-    { name = "path" }, -- for path completion
-    { name = "buffer", keyword_length = 2 }, -- for buffer word completion
-  },
-  completion = {
-    keyword_length = 1,
-    completeopt = "menu,noselect",
-  },
-  view = {
-    entries = "custom",
-  },
-  formatting = {
-    format = lspkind.cmp_format {
-      mode = "symbol_text",
-      menu = {
-        nvim_lsp = "[LSP]",
-        ultisnips = "[US]",
-        path = "[Path]",
-        buffer = "[Buffer]",
-        emoji = "[Emoji]",
-        omni = "[Omni]",
-      },
-      show_labelDetails = true,
-      maxwidth = 40,
-      ellipsis_char = "...",
-    },
-  },
-}
+require("cmp_nvim_lsp")
+require("cmp_path")
+require("cmp_buffer")
+require("cmp_omni")
+require("cmp_nvim_ultisnips")
+
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			-- For `ultisnips` user.
+			vim.fn["UltiSnips#Anon"](args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<Tab>"] = function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			else
+				fallback()
+			end
+		end,
+		["<S-Tab>"] = function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			else
+				fallback()
+			end
+		end,
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<Esc>"] = cmp.mapping.close(),
+		["<C-d>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+	}),
+	sources = {
+		{ name = "nvim_lsp" }, -- For nvim-lsp
+		{ name = "ultisnips" }, -- For ultisnips user.
+		{ name = "path" }, -- for path completion
+		{ name = "buffer", keyword_length = 2 }, -- for buffer word completion
+	},
+	completion = {
+		keyword_length = 1,
+		completeopt = "menu,noselect",
+	},
+	view = {
+		entries = "custom",
+	},
+	-- solution taken from https://github.com/echasnovski/mini.nvim/issues/1007#issuecomment-2258929830
+	formatting = {
+		format = function(_, vim_item)
+			local icon, hl = MiniIcons.get("lsp", vim_item.kind)
+			vim_item.kind = icon .. " " .. vim_item.kind
+			vim_item.kind_hl_group = hl
+			return vim_item
+		end,
+	},
+})
 
 cmp.setup.filetype("tex", {
-  sources = {
-    { name = "omni" },
-    { name = "ultisnips" }, -- For ultisnips user.
-    { name = "buffer", keyword_length = 2 }, -- for buffer word completion
-    { name = "path" }, -- for path completion
-  },
+	sources = {
+		{ name = "omni" },
+		{ name = "ultisnips" }, -- For ultisnips user.
+		{ name = "buffer", keyword_length = 2 }, -- for buffer word completion
+		{ name = "path" }, -- for path completion
+	},
 })
 
 --  see https://github.com/hrsh7th/nvim-cmp/wiki/Menu-Appearance#how-to-add-visual-studio-code-dark-theme-colors-to-the-menu
