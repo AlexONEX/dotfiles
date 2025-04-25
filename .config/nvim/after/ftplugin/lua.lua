0002a043c2f2315669abf4014f4a2e6dc485306e
AlexONEX <22077128+AlexONEX@users.noreply.github.com>
2025-04-24 23:52:05 -0300

    refactor(nvim): update ftplugins to use modern APIs
    - Replace deprecated vim.api.nvim_buf_set_keymap with vim.keymap.set
    - Replace vim.api.nvim_err_writeln with vim.notify
    - Fix formatting and local variable scoping
    - Update C++ standard to C++17
    - Improve error handling in language runners

--- .config/nvim/after/ftplugin/lua.lua
+++ .config/nvim/after/ftplugin/lua.lua
@@ -2,24 +2,24 @@ vim.bo.expandtab = true
 vim.bo.shiftwidth = 2
 vim.bo.softtabstop = 2
 vim.bo.tabstop = 2
-vim.opt_local.formatoptions:remove({ "o", "r" })
+vim.opt_local.formatoptions:remove { "o", "r" }
 
 if not _G.format_and_save then
-	_G.format_and_save = function()
-		vim.cmd("silent !stylua %")
-		vim.cmd("edit")
-		vim.cmd("write")
-	end
+  _G.format_and_save = function()
+    vim.cmd("silent !stylua %")
+    vim.cmd("edit")
+    vim.cmd("write")
+  end
 end
 
 if not _G.run_lua then
-	_G.run_lua = function()
-		vim.cmd("luafile %")
-	end
+  _G.run_lua = function()
+    vim.cmd("luafile %")
+  end
 end
 
 -- Key mappings
 local opts = { buffer = true, silent = true }
 vim.keymap.set("n", "<C-s>", _G.format_and_save, opts)
 vim.keymap.set("n", "<F9>", _G.run_lua, opts)
-vim.keymap.set("n", "<space>f", "<cmd>silent !stylua %<CR>", opts)
+vim.keymap.set("n", "<space>f", _G.format_and_save, opts)
--- .config/nvim/after/ftplugin/toml.lua
+++ .config/nvim/after/ftplugin/toml.lua
@@ -7,5 +7,4 @@ local function format_and_save()
 end
 
 vim.api.nvim_create_user_command("FormatAndSaveToml", format_and_save, {})
-
 vim.api.nvim_buf_set_keymap(0, "n", "<C-s>", ":FormatAndSaveToml<CR>", { noremap = true, silent = true })
