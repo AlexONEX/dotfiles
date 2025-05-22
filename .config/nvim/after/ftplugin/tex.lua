48576bf4a5bdaa8657822a7a2ebf9658d81b42db
AlexONEX <22077128+AlexONEX@users.noreply.github.com>
2025-05-22 19:54:39 -0300

    fix(nvim) - disable treesitter highlight to fix concealment - closes #11

--- .config/nvim/after/ftplugin/tex.lua
+++ .config/nvim/after/ftplugin/tex.lua
@@ -1,44 +1,62 @@
 -- Load VimTeX configuration if available
 if vim.fn.filereadable(vim.fn.stdpath("config") .. "/lua/config/vimtex.lua") then
-	local vimtex_config = require("config.vimtex")
-	vimtex_config.setup()
+  local vimtex_config = require("config.vimtex")
+  vimtex_config.setup()
 end
 
--- buffer
+-- Disable TreeSitter highlight for LaTeX files (para que funcione concealment de VimTeX)
+vim.cmd("TSBufDisable highlight")
+
+-- Configurar concealment específico para LaTeX
+vim.api.nvim_set_option_value("conceallevel", 2, { scope = "local" })
+vim.api.nvim_set_option_value("concealcursor", "nc", { scope = "local" })
+
+-- Buffer configuration
 local function setup_buffer()
-	-- Configuración de texto y formato
-	vim.opt_local.textwidth = 120
-	vim.opt_local.wrap = true
-	vim.opt_local.linebreak = true
+  -- Configuración de texto y formato
+  vim.opt_local.textwidth = 120
+  vim.opt_local.wrap = true
+  vim.opt_local.linebreak = true
+  vim.opt_local.formatoptions:remove { "o", "r" }
 
-	vim.opt_local.formatoptions:remove({ "o", "r" })
+  -- indent
+  vim.opt_local.expandtab = true
+  vim.opt_local.shiftwidth = 2
+  vim.opt_local.tabstop = 2
+  vim.opt_local.softtabstop = 2
 
-	-- indent
-	vim.opt_local.expandtab = true
-	vim.opt_local.shiftwidth = 2
-	vim.opt_local.tabstop = 2
-	vim.opt_local.softtabstop = 2
-
-	-- Spell checking
-	vim.opt_local.spell = true
-	vim.keymap.set("i", "<A-l>", "<c-g>u<Esc>[s1z=`]a<c-g>u", { noremap = true, silent = true })
-	vim.opt_local.spelllang = "es,en"
+  -- Spell checking
+  vim.opt_local.spell = true
+  vim.keymap.set("i", "<A-l>", "<c-g>u<Esc>[s1z=`]a<c-g>u", { noremap = true, silent = true, buffer = true })
+  vim.opt_local.spelllang = "es,en"
 end
 
 -- Format and save
 local function format_and_save()
-	vim.lsp.buf.format()
-	vim.cmd("write")
+  vim.lsp.buf.format()
+  vim.cmd("write")
 end
 
 -- Configurar keymaps genéricos para LaTeX
 local function setup_latex_keymaps()
-	-- Guardar y formatear
-	vim.keymap.set("n", "<C-s>", format_and_save, { buffer = true, silent = true })
+  -- Guardar y formatear
+  vim.keymap.set("n", "<C-s>", format_and_save, { buffer = true, silent = true })
 
-	-- Snippets y completion (si no usas VimTeX para esto)
-	vim.keymap.set("i", ";;", "\\", { buffer = true })
-	vim.keymap.set("i", "$$", "$$ $$<left><left><left>", { buffer = true })
+  -- Toggle concealment específico para este buffer
+  vim.keymap.set("n", "<leader>lh", function()
+    local current_level = vim.api.nvim_get_option_value("conceallevel", { scope = "local" })
+    if current_level == 0 then
+      vim.api.nvim_set_option_value("conceallevel", 2, { scope = "local" })
+      vim.notify("Concealment enabled")
+    else
+      vim.api.nvim_set_option_value("conceallevel", 0, { scope = "local" })
+      vim.notify("Concealment disabled")
+    end
+  end, { buffer = true, silent = true })
+
+  -- Snippets y completion (si no usas VimTeX para esto)
+  vim.keymap.set("i", ";;", "\\", { buffer = true })
+  vim.keymap.set("i", "$$", "$$ $$<left><left><left>", { buffer = true })
 end
 
 setup_buffer()
--- .config/nvim/lua/config/vimtex.lua
+++ .config/nvim/lua/config/vimtex.lua
@@ -4,14 +4,14 @@ local function setup_keymaps()
   local keymaps = {
     { "n", "<F9>", "<plug>(vimtex-compile)", { buffer = true } },
     { "n", "<leader>ll", "<cmd>VimtexCompile<CR>", { buffer = true } },
-    -- Visualización
+    -- Visualization
     { "n", "<leader>lv", "<cmd>VimtexView<CR>", { buffer = true } },
     { "n", "<leader>le", "<cmd>VimtexErrors<CR>", { buffer = true } },
     -- TOC
     { "n", "<leader>lt", "<cmd>VimtexTocToggle<CR>", { buffer = true } },
-    -- Limpieza
+    -- Clean
     { "n", "<leader>lc", "<cmd>VimtexClean<CR>", { buffer = true } },
-    -- Otros
+    -- Other
     { "n", "<leader>li", "<cmd>VimtexInfo<CR>", { buffer = true } },
     { "n", "<leader>lk", "<cmd>VimtexStop<CR>", { buffer = true } },
     { "n", "<leader>lK", "<cmd>VimtexStopAll<CR>", { buffer = true } },
@@ -26,11 +26,13 @@ local function setup_autocommands()
   vim.api.nvim_create_autocmd("FileType", {
     pattern = "tex",
     callback = function()
-      if vim.fn.exists("*vimtex#init") == 1 then
-        vim.notify("VimTeX loaded successfully")
-      else
-        vim.notify("VimTeX not loaded!", vim.log.levels.ERROR)
-      end
+      vim.defer_fn(function()
+        if vim.fn.exists("*vimtex#init") == 1 then
+          vim.notify("VimTeX loaded successfully", vim.log.levels.INFO)
+        else
+          vim.notify("VimTeX not loaded!", vim.log.levels.ERROR)
+        end
+      end, 100)
     end,
   })
 end
@@ -44,13 +46,30 @@ local function setup_ultisnips()
 end
 
 function M.setup()
+  -- Basic config
   vim.g.tex_flavor = "latex"
   vim.g.vimtex_view_method = "zathura"
   vim.g.vimtex_view_general_options = "--unique file:@pdf\\#src:@line@tex"
 
-  vim.opt.conceallevel = 1
-  vim.g.tex_conceal = "abdmg"
+  -- Concealment
+  vim.g.tex_conceal = "abdmgs"
+  vim.g.vimtex_syntax_conceal = {
+    accents = 1,
+    ligatures = 1,
+    cites = 1,
+    fancy = 1,
+    spacing = 1,
+    greek = 1,
+    math_bounds = 1,
+    math_delimiters = 1,
+    math_fracs = 1,
+    math_super_sub = 1,
+    math_symbols = 1,
+    sections = 0,
+    styles = 1,
+  }
 
+  -- Compiler
   vim.g.vimtex_compiler_method = "latexmk"
   vim.g.vimtex_compiler_latexmk = {
     build_dir = "build",
@@ -66,13 +85,15 @@ function M.setup()
       "-pdf",
     },
   }
-
   vim.g.vimtex_compiler_progname = "nvr"
   vim.g.vimtex_compiler_enabled = 1
   vim.g.vimtex_compiler_silent = 0
+
+  -- Visualización
   vim.g.vimtex_view_enabled = 1
   vim.g.vimtex_log_verbose = 1
 
+  -- TOC
   vim.g.vimtex_toc_config = {
     name = "TOC",
     layers = { "content", "todo", "include" },
@@ -84,8 +105,8 @@ function M.setup()
     mode = 2,
   }
 
+  -- Other config
   vim.g.vimtex_compiler_latexmk.root_patterns = { ".latexmkrc", "main.tex", ".git", "." }
-
   vim.g.vimtex_quickfix_mode = 0
   vim.g.vimtex_quickfix_enabled = 1
   vim.g.vimtex_quickfix_ignore_filters = {
@@ -96,14 +117,13 @@ function M.setup()
     "Package typearea Warning",
   }
 
+  -- Sintax
   vim.g.vimtex_syntax_enabled = 1
   vim.g.vimtex_fold_enabled = 0
   vim.g.vimtex_format_enabled = 1
   vim.g.vimtex_complete_enabled = 1
-  vim.g.loaded_vimtex = 1
   vim.g.vimtex_enabled = 1
   vim.g.vimtex_mappings_disable = { ["n"] = { "K" } }
-
   vim.g.vimtex_importer_enabled = 1
 
   setup_ultisnips()
