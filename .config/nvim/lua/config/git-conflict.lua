21d423468359896456db6995881053980dc060cb
AlexONEX <22077128+AlexONEX@users.noreply.github.com>
2025-06-24 11:57:54 -0300

    fix(nvim) - gitconflict plugin

--- .config/nvim/lua/config/git-conflict.lua
+++ .config/nvim/lua/config/git-conflict.lua
@@ -1,12 +1,11 @@
 require("git-conflict").setup({})
 
 vim.api.nvim_create_autocmd("User", {
-	pattern = "GitConflictResolved",
-	callback = function()
-		-- clear qf list
-		vim.fn.setqflist({}, "r")
-
-		-- reopen it?
-		vim.cmd([[GitConflictListQf]])
-	end,
+    pattern = "GitConflictResolved",
+    callback = function()
+        vim.schedule(function()
+            vim.fn.setqflist({}, "r")
+            vim.cmd([[GitConflictListQf]])
+        end)
+    end,
 })
